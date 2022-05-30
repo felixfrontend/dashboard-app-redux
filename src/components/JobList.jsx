50bea83b74a44851d1ca7f8baf9e76a7b9ab1339
Пользователь <felixkyrgyzstan@gmail.com>
2022-05-31 05:57:42 +0600

    dashboard created

---
+++ src/components/JobList.jsx
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import { JobPosition } from "./JobPosition";
+import { selectVisiblePositions } from "store/positions/position-selectors";
+import { addFilter } from "store/filters/filter-actions";
+import { selectFilters } from "store/filters/filter-selector";
+
+const JobList = () => {
+  const dispatch = useDispatch();
+  const currentFilters = useSelector(selectFilters);
+  const positions = useSelector((state) =>
+    selectVisiblePositions(state, currentFilters)
+  );
+
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
+  };
+
+  return (
+    <div className="job-list">
+      {positions.map((item) => (
+        <JobPosition
+          key={item.id}
+          handleAddFilter={handleAddFilter}
+          {...item}
+        />
+      ))}
+    </div>
+  );
+};
+
+export { JobList };
